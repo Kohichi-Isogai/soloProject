b866cd630fa810ecc39362fda27d6d6988630c2d
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-05-23 15:14:31 +0900

    全データテーブル作成　-seedはまだ

---
+++ db/migrations/20240523060141_second_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("my_friend", (table) => {
+    table.increments("id").primary();
+    table.string("user").unique();
+    table.integer("friend_id").references("friend.id");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("my_friend");
+};
---
+++ db/migrations/20240523061139_third_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("enemy", (table) => {
+    table.increments("id").primary();
+    table.string("name").unique();
+    table.integer("attack");
+    table.integer("hp");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("enemy");
+};
