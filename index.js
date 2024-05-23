8fb819ac0e102827693abc494f0fbdbe180c9076
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-05-23 16:13:46 +0900

    add : server作成 get /gacha

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const knex = require("knex")(config);
 app.listen(3000, () => {
   console.log("http://localhost:3000");
 });
+
+app.get("/gacha", async (req, res) => {
+  const result = await knex("friend").select();
+  res.status(200).send(result[Math.floor(Math.random() * 3)]);
+});
